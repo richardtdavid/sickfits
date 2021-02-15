6c5f281a1acd5ef53887cc2fb500985d43595c6a
Richard <driichard@gmail.com>
2021-02-15 17:14:01 -0600

    yeet

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,18 +1,25 @@
-import NProgress from 'nprogress';
+import NProgresting from 'nprogress';
 import Router from 'next/router';
 import Page from '../components/page';
 // TODO: swap with our own
 // import 'nprogress/nprogress.css';
 import '../components/styles/nprogress.css';
+import {ApolloProvider} from '@apollo/client';
+import withData from '../lib/withData';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo}) {
+  console.log("testing::")
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+export default withData(MyApp)
